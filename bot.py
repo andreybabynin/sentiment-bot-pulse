6af020fe2aec039bbb730a105350786ee4be7a23
andrey <enron07@gmail.com>
2021-09-30 17:22:30 +0300

    add basic bot

---
+++ bot.py
@@ -0,0 +1,36 @@
+import requests as re
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import json
+
+telegram_bot_token = '1284331928:AAFbIt-HNuEhX1uDExXgVyXOvY_PgYXBkxo'
+
+
+def recent_posts(update, context):
+    # fetch data from the api
+    #response = requests.get('http://quotes.stormconsultancy.co.uk/random.json')
+    #data = response.json()
+    # send message
+    if len(context.args)==0:
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Ticker is not provided')
+    else:
+        ticker = context.args[0]
+        link='https://www.tinkoff.ru/api/invest-gw/social/v1/post/instrument/{}?limit=30&appName=invest&platform=web'.format(ticker)
+        response = re.get(link)
+        
+        if response.status_code == 200:
+            dic = json.loads(response.text)
+            items = dic['payload']['items']
+            for i in range(3):
+                text = items[i]['text']
+                min_v = min(len(text), 100)
+                context.bot.send_message(chat_id=update.effective_chat.id, text=text[:min_v]) 
+        else: 
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Try later')
+
+
+if __name__ == '__main__':
+    updater = Updater(token=telegram_bot_token, use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('recent', recent_posts))
+    updater.start_polling()
+    updater.idle()
